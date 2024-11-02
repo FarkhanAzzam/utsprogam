2af6d3fe3c583e02485da2b959cb0ddb26c7a672
FarkhanAzzam <daisyjohnson1210@gmail.com>
2024-11-02 23:58:32 +0700

    Add files via upload

---
+++ BMIberaksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author daisy
+ */
+public class BMIberaksi {
+    public static void main(String[] args) {
+        BMI bmi = new BMI ();
+        bmi.kalkulatorBMI(1.69, 49);
+    }
+}
